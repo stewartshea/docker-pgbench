3b17340d38dc69beffae8cc0ac93067b33d049a0
Shea Stewart <sheastewart@sheasus.local>
2018-12-03 14:22:37 -0800

    test random string

--- hammerdb/pgsql_build.tcl
+++ hammerdb/pgsql_build.tcl
@@ -1,4 +1,13 @@
 #!/bin/tclsh
+puts "GENERATING TEST DB NAME"
+proc rndpassword len {
+ set s "abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ23456789"
+ for {set i 0} {$i <= $len} {incr i} {
+    append p [string index $s [expr {int([string length $s]*rand())}]]
+ }
+ return $p
+}
+puts "TEST DB is $p"
 puts "SETTING CONFIGURATION"
 global complete
 proc wait_to_complete {} {
@@ -9,7 +18,7 @@ if {!$complete} {after 5000 wait_to_complete} else { exit }
 dbset db pg
 diset connection pg_host $::env(PGHOST)
 diset connection pg_port 5432
-diset connection pg_dbase $::(PG_TEST_DBASE)
+diset connection pg_dbase $p
 dbset bm TPC-C
 print dict
 buildschema
