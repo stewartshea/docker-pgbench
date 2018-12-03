2163a3550c8faf13c90dcd0a5f3032d4e84b9461
Shea Stewart <sheastewart@sheasus.local>
2018-12-03 13:36:43 -0800

    x

--- hammerdb/pgsql_build.tcl
+++ hammerdb/pgsql_build.tcl
@@ -1,5 +1,4 @@
 #!/bin/tclsh
-set pghost $::env(PG_HOST)
 puts "SETTING CONFIGURATION"
 global complete
 proc wait_to_complete {} {
@@ -8,7 +7,7 @@ set complete [vucomplete]
 if {!$complete} {after 5000 wait_to_complete} else { exit }
 }
 dbset db pg
-diset connection pg_host $pghost
+diset connection pg_host $::env(PGHOST)
 diset connection pg_port 5432
 dbset bm TPC-C
 print dict
