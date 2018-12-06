3725caba110a8762e56c2fe49b63318ed649f396
Shea Stewart <sheastewart@sheasus.local>
2018-12-05 16:18:04 -0800

    x

--- hammerdb/hammerdb.sh
+++ hammerdb/hammerdb.sh
@@ -2,12 +2,15 @@
 echo 'GENERATING 6 CHARACTER RANDOM TEST NAME'
 export TEST_NAME=$(cat /dev/urandom | tr -dc 'a-z' | fold -w 6 | head -n 1)
 
-echo 'STARTING SCHEMA BUILD'
+# echo 'STARTING SCHEMA BUILD'
+# ./hammerdbcli <<!
+# source pgsql_build.tcl
+# !
+# echo 'SCHEMA BUILD COMPLETE'
+echo 'STARTING TEST'
 ./hammerdbcli <<!
-source pgsql_build.tcl
+source pgsql_driver_test.tcl
 !
-echo 'SCHEMA BUILD COMPLETE'
-
 
 echo 'TEST COMPLETE'
 
